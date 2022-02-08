23643dac48cf6c2bf10ab6687941c787a5dd0b50
Arunima Chatterjee <92734728+Arunima-Chatterjee@users.noreply.github.com>
2022-02-08 18:55:14 +0530

    Uploading .py file

---
+++ pyt/Basic Data Types/hello world.py
@@ -0,0 +1,3 @@
+
+#Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second-lowest grade.**
+print("hello world")
