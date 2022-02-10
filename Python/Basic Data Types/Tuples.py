1afe4a9ce6d4c532efec52e7dd1e637d50f17e55
Arunima Chatterjee <92734728+Arunima-Chatterjee@users.noreply.github.com>
2022-02-10 13:29:18 +0530

    Added Tuples.py

---
+++ Python/Basic Data Types/Tuples.py
@@ -0,0 +1,11 @@
+# Given an integer , n and n space-separated integers as input, create
+# a tuple, t of those n integers. Then compute and print the result of hash(t) .
+
+# Note : hash() is one of the functions in the __builtins__ module,
+# so it need not be imported.
+
+if __name__ == '__main__':
+    n = int ( input ( ) )
+    t = tuple(map(int , input().split(' ')))
+    h = hash ( t )
+    print( h )
