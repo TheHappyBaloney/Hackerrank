f62b870ace9f1f0f067401f865d32c53f50b7e99
Arunima Chatterjee <92734728+Arunima-Chatterjee@users.noreply.github.com>
2022-04-08 10:36:20 +0530

    Update collections.counter( ).py

--- Python/Collections/collections.counter( ).py
+++ Python/Collections/collections.counter( ).py
@@ -1,3 +1,7 @@
+# Raghu  is a shoe shop owner. His shop has X number of shoes. He has a list containing the size of each shoe he has in his shop.
+# There are N number of customers who are willing to pay x amount of money only if they get the shoe of their desired size. 
+# Your task is to compute how much money Raghu earned.
+
 from collections import Counter
 x = input( )
 S = Counter(map(int,input( ).split( )))
